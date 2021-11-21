f2708f461a7a023e8ba04428f96cbb1d9fcdcd7b
kapselccc <s184848@student.pg.edu.pl>
2021-11-21 18:58:16 +0100

    adding dog type and cat type in bootstrap

--- src/main/java/kpc/springframework/springpetclinic/bootstrap/DataLoader.java
+++ src/main/java/kpc/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package kpc.springframework.springpetclinic.bootstrap;
 
 import kpc.springframework.springpetclinic.model.Owner;
+import kpc.springframework.springpetclinic.model.PetType;
 import kpc.springframework.springpetclinic.model.Vet;
 import kpc.springframework.springpetclinic.services.OwnerService;
+import kpc.springframework.springpetclinic.services.PetTypeService;
 import kpc.springframework.springpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Karol");
         owner1.setLastName("Mis");
